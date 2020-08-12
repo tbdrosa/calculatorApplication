f4c4031b56e508bed951f67c3371c03a3cdfc485
tbdrosa <tbdrosa@gmail.com>
2020-08-11 20:21:53 -0400

    added ~javaScript constants~ for all html contents (app.js)

---
+++ app.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelectorAll('[data-equals]');
+const clearButton = document.querySelector('data-all-clear');
+const deleteButton = document.querySelector('data-delete');
+const previousOpperandTextElement = document.querySelector('data-previous-opperand ');
+const currentOpperandTextElement = document.querySelector('data-current-opperand');
