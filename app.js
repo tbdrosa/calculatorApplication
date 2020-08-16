4339e4cc7f7d3edd0af688b9d3f6a4bacdfd096f
tbdrosa <tbdrosa@gmail.com>
2020-08-16 18:06:08 -0400

    added ~constructor~ function (app.js)

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+}
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelectorAll('[data-equals]');
 const clearButton = document.querySelector('data-all-clear');
 const deleteButton = document.querySelector('data-delete');
-const previousOpperandTextElement = document.querySelector('data-previous-operand ');
-const currentOpperandTextElement = document.querySelector('data-current-operand');
+const previousOperandTextElement = document.querySelector('data-previous-operand ');
+const currentOperandTextElement = document.querySelector('data-current-operand');
+
