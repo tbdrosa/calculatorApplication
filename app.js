790da8317570146170565a3d9975700447fc92cb
tbdrosa <tbdrosa@gmail.com>
2020-08-16 19:12:54 -0400

    added ~all-clear button function elements~ (app.js)

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ class Calculator{
     constructor(previousOperandTextElement, currentOperandTextElement){
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
     
     clear(){
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.Operation = undefined
         
     }
     
