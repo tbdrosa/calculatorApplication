46819dd5cfb6a8df518328223090ba51f94884c0
tbdrosa <tbdrosa@gmail.com>
2020-08-16 18:00:12 -0400

    fixed more spelling errors ~operand~ (all files)

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelectorAll('[data-equals]');
 const clearButton = document.querySelector('data-all-clear');
 const deleteButton = document.querySelector('data-delete');
-const previousOpperandTextElement = document.querySelector('data-previous-opperand ');
-const currentOpperandTextElement = document.querySelector('data-current-opperand');
+const previousOpperandTextElement = document.querySelector('data-previous-operand ');
+const currentOpperandTextElement = document.querySelector('data-current-operand');
